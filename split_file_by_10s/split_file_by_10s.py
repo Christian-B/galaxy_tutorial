4e464799b9e9282ee46a4ebdd12a43f6f3f7d1db
= <christian.brenninkmeijer@manchester.ac.uk>
2016-04-20 17:44:40 +0100

    Added discovered datasets example

---
+++ split_file_by_10s/split_file_by_10s.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Python version of tail function.')
+    parser.add_argument('--input', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    all_lines = args.input.readlines()
+    if not os.path.exists("split"):
+        os.makedirs("split")
+    for ten in range(0,len(all_lines)/10):
+        use_lines =  all_lines[ten*10:(ten+1)*10]
+        with open("split/lines"+str(ten)+".txt", 'w') as f:
+            for line in use_lines:
+                f.write(line)
+        sys.stdout.write("Ten file saved as output/lines"+str(ten)+".txt\n")
+
